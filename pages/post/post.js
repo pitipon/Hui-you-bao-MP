e0abaaad424647997be299020b0e6555775b2b27
pitipon <pitipon@gmail.com>
2017-12-06 22:26:57 +0800

    add more visual for post and profile

--- pages/post/post.js
+++ pages/post/post.js
@@ -1,22 +1,29 @@
 // pages/post/post.js
-
-
-
 Page({
 
+  //   var pageData = {}
+  // for (var i = 1; i< 5; i++) {
+  //   (function (index) {
+  //     pageData['slider' + index + 'change'] = function (e) {
+  //       console.log('slider' + 'index' + '发生 change 事件，携带值为', e.detail.value)
+  //     }
+  //   })(i)
+  // }
+  // Page(pageData)
   /**
    * 页面的初始数据
    */
   data: {
     haveImage: false,
-    imageSrc: ""
+    imageSrc: "",
+    categories: ['food', 'toys', 'books', 'bikes', 'animals'],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
@@ -31,7 +38,7 @@ Page({
       success: function (res) {
         // Returns the local file path list for the selected photo, tempFilePath can be used as the img tag's src attribute to display the image
         var tempFilePaths = res.tempFilePaths
-        
+
         console.log(tempFilePaths)
         that.setData({
           haveImage: true,
@@ -54,41 +61,45 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
+
+  // switch1Change: function (e) {
+  //   console.log('switch1 发生 change 事件，携带值为', e.detail.value)
+  // }
 })
--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,66 +1,50 @@
-// pages/profile/profile.js
+
+var app = getApp();
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    gridList: [
+      { enName: 'box1', zhName: 'salmon <3er' },
+      {},
+      {},
+      {},
+      {},
+      {},
+      {},
+      {},
+      {},
+    ],
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  onLoad: function (cb) {
+    var that = this
+    console.log(app.globalData.userInfo)
+
+    if (app.globalData.userInfo != null) {
+      that.setData({
+        userInfo: app.globalData.userInfo
+      })
+    } else {
+      app.getUserInfo()
+    }
+    typeof cb == 'function' && cb()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function () {
-  
+    this.onLoad(function () {
+      wx.stopPullDownRefresh()
+    })
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
+  viewGridDetail: function (e) {
+    var data = e.currentTarget.dataset
+    wx.navigateTo({
+      url: "../" + data.url + '/' + data.url
+    })
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
+
+
+
+
+
